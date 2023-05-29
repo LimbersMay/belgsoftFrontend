bd82c3b96a5a1e236204a2b2a1105f91bb0a9b1d
limberMay <limbermay1@gmail.com>
2023-05-29 15:13:51 -0600

    (src/data/waiter) productRepository creado para solicitar datos a la API del backend

---
+++ src/data/waiter/product.repository.js
@@ -0,0 +1,12 @@
+
+export const productRepository = async (token, product) => {
+    const response = await fetch(`http://localhost:3000/api/menu/${product}`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'x-token': token
+        }
+    });
+
+    return await response.json();
+}
