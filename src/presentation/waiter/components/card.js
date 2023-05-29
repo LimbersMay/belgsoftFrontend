9ee61c83cd6e5a76d06e78a8b6c05b515d01d99c
limberMay <limbermay1@gmail.com>
2023-05-29 16:12:55 -0600

    (src/presentation/waiter) componente card para los productos creado

---
+++ src/presentation/waiter/components/card.js
@@ -0,0 +1,16 @@
+
+export const createProductCard = (title, imgUrl) => {
+    return `
+        <div class="food-container">
+            <section>
+                <h2>${title}</h2> <!-- with js -->
+                <div class="menu-item-with-grid">
+                    <div class="element"><img src=${imgUrl} alt=${title}></div>
+                    <div class="element"><label for="input">Cantidad: </label></div>
+                    <div class="element"><input class="input-text input-text2" id="input" type="text" value="0"></div>
+                </div>
+            </section>
+            
+        </div>
+    `
+}
