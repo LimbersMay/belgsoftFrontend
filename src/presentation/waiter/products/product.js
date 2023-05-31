3ba52fdbf74a4e1ed62c8caa6e24fcbbe6fd9d1b
limberMay <limbermay1@gmail.com>
2023-05-30 20:21:56 -0600

    (presentation/waiter product) archivo javascript para renderizar todos los productos de una categoria creado

---
+++ src/presentation/waiter/products/product.js
@@ -0,0 +1,31 @@
+// we fetch data from the domain
+import {processProduct} from "../../../domain/waiter/process-product.js";
+import {createProductCard} from "./components/ProductCard.js";
+
+(async () => {
+
+    const titles = {
+        'foods': 'Comida',
+        'drinks': 'Bebida',
+        'dessert': 'Postre'
+    }
+
+    // check if url contains a query string
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const productType = urlParams.get('type');
+
+    // set the title
+    const title = document.querySelector('#product-title');
+    title.innerHTML = titles[productType];
+
+    const foods = await processProduct(productType);
+    const foodContainer = document.querySelector('.product-container');
+
+    foods.forEach(food => {
+        const foodCard = createProductCard(food.name, food.image);
+
+        // insert before the end of the container
+        foodContainer.insertAdjacentHTML('afterbegin', foodCard);
+    });
+})();
