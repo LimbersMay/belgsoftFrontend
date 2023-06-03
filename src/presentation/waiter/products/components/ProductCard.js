a55cb8e7ccded7b2216f483122821ef347bd380f
limberMay <limbermay1@gmail.com>
2023-06-03 13:44:49 -0600

    (presentation/waiter productCard) label for e input valores reemplazados por el title recibido

--- src/presentation/waiter/products/components/ProductCard.js
+++ src/presentation/waiter/products/components/ProductCard.js
@@ -6,8 +6,8 @@ export const createProductCard = (id, title, imgUrl) => {
                 <h2>${title}</h2> <!-- with js -->
                 <div class="menu-item-with-grid">
                     <div class="element"><img src=${imgUrl} alt=${title}></div>
-                    <div class="element"><label for="input">Cantidad: </label></div>
-                    <div class="element"><input class="input-text input-text2" id="input" type="text" value="0"></div>
+                    <div class="element"><label for="${title}">Cantidad: </label></div>
+                    <div class="element"><input class="quantity input-text input-text2" id="${title}" type="text" value="0"></div>
                 </div>
             </section>
         </div>
