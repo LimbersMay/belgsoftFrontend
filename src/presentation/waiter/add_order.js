857706d25efe54f22aa79437e707fab07ed20df1
limberMay <limbermay1@gmail.com>
2023-06-03 16:10:50 -0600

    (presentation/waiter add_order) productos del resumen de pedidos obtenidos desde el localStorage en lugar de la base de datos

--- src/presentation/waiter/add_order.js
+++ src/presentation/waiter/add_order.js
@@ -1,27 +1,19 @@
 /** Work in progress **/
-
 import {getProductInCartById, getProductsInCart} from "../../domain/waiter/getProductsInCart.js";
-import {processProductFromApi} from "../../domain/waiter/processProductsFromApi.js";
 import {createProductListItem} from "./components/ProductItem.js";
 
 (async () => {
 
     const productsInCart = await getProductsInCart();
-
     if (!productsInCart) return;
 
-    const productsFetchFromApi = Promise.all(
-        productsInCart.map(async ({ menuId }) => processProductFromApi(menuId))
-    )
-
-    const productsData = await productsFetchFromApi;
     const productsListContainer = document.querySelector('#productsList');
 
     let total = 0;
 
-    for (const {menuId, name, price} of productsData) {
+    for (const {menuId, title, price} of productsInCart) {
         const { quantity } = await getProductInCartById(menuId);
-        const productListItem = createProductListItem(name, quantity, price);
+        const productListItem = createProductListItem(title, quantity, price);
 
         total += price * quantity;
         productsListContainer.insertAdjacentHTML('afterbegin', productListItem);
