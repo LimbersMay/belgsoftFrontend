f9da3a7549dd83dcc5560bddc3fdbb1166a948dc
limberMay <limbermay1@gmail.com>
2023-06-03 15:45:38 -0600

    (presentation/waiter add_order) Se agregó la función para cargar todas las órdenes y el total en el menu principal del mesero

--- src/presentation/waiter/add_order.js
+++ src/presentation/waiter/add_order.js
@@ -1,1 +1,33 @@
 /** Work in progress **/
+
+import {getProductInCartById, getProductsInCart} from "../../domain/waiter/getProductsInCart.js";
+import {processProductFromApi} from "../../domain/waiter/processProductsFromApi.js";
+import {createProductListItem} from "./components/ProductItem.js";
+
+(async () => {
+
+    const productsInCart = await getProductsInCart();
+
+    if (!productsInCart) return;
+
+    const productsFetchFromApi = Promise.all(
+        productsInCart.map(async ({ menuId }) => processProductFromApi(menuId))
+    )
+
+    const productsData = await productsFetchFromApi;
+    const productsListContainer = document.querySelector('#productsList');
+
+    let total = 0;
+
+    for (const {menuId, name, price} of productsData) {
+        const { quantity } = await getProductInCartById(menuId);
+        const productListItem = createProductListItem(name, quantity, price);
+
+        total += price * quantity;
+        productsListContainer.insertAdjacentHTML('afterbegin', productListItem);
+    }
+
+    // Set the total
+    const priceElement = document.querySelector('#total');
+    priceElement.innerHTML = `Total: $${total}`;
+})()
