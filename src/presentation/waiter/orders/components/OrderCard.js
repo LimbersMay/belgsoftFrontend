785fcb6c5e308a53432af77e4def8cc74c8375b1
limberMay <limbermay1@gmail.com>
2023-06-06 10:12:09 -0600

    (presentation/waiter/components OrderCard) función que crea un orderCard creado

---
+++ src/presentation/waiter/orders/components/OrderCard.js
@@ -0,0 +1,24 @@
+
+export const createOrderCard = (area, tableNumber, orderStatus, waiterName, productQuantity, check) => {
+    return `
+        <div  class="container order-container">   
+            <p id="order">Area: <span>${area}</span></p>
+            <hr>
+            <p class="waiter">Mesero: <span>${waiterName}</span></p>
+            <p class="waiter">Estado: <span>${orderStatus}</span></p>
+            <div class="grid order-container2">
+                <div class="table-area">
+                    <p id="table">Mesa: <span>${tableNumber}</span></p>
+                </div>
+                <div class="product-total">
+                    <p>Productos: <span>${productQuantity}</span></p>
+                    <p id="total">Total: <span>${check}</span></p>
+                </div>
+                <div class="event l">
+                    <a class="button1" href="details.html">Detalles</a>
+                    <a class="button1 colorbb" href="#">Solicitar cancelación</a>
+                </div>
+            </div>
+        </div>
+    `
+}
