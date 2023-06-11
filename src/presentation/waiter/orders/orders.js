795aded982effddeffacff79236cf99f3f26c418
limberMay <limbermay1@gmail.com>
2023-06-11 07:43:10 -0600

    (presentation/waiter/orders orders.js) Se obtuvieron todas las órdenes por mesa

--- src/presentation/waiter/orders/orders.js
+++ src/presentation/waiter/orders/orders.js
@@ -5,25 +5,26 @@ import {createOrderCard} from "./components/OrderCard.js";
 
     const userOrders = await processUserOrdersFromApi();
 
-    /*
-        orderExample:
-            {
-            "orderId": "0e7ab500-8091-44f2-afd7-64c0664a5abf",
-            "customerName": null,
-            "tableNumber": "5",
-            "userName": "Bernard",
-            "orderStatus": "PENDING",
-            "area": "Area 1",
-            "price": 156,
-            "quantity": 210,
-            "createdAt": "2023-06-04T06:53:12.000Z"
+    const usersOrdersReduced = Object.values(userOrders.reduce((accumulator, currentObj) => {
+        const { tableNumber, price, quantity, ...rest } = currentObj;
+        if (!accumulator[tableNumber]) {
+            accumulator[tableNumber] = {
+                ...rest,
+                tableNumber,
+                price,
+                quantity
+            };
+        } else {
+            accumulator[tableNumber].price += price;
+            accumulator[tableNumber].quantity += quantity;
         }
-     */
+        return accumulator;
+    }, {}));
 
     const ordersContainer = document.querySelector('main');
 
-    for (let { area, tableNumber, orderStatus, userName, quantity, price} of userOrders) {
-        const orderCard = createOrderCard(area, tableNumber, orderStatus, userName, quantity, price)
+    for (let { area, tableNumber, orderStatus, userName, quantity, price, tableId} of usersOrdersReduced) {
+        const orderCard = createOrderCard(area, tableNumber, orderStatus, userName, quantity, price, tableId )
         ordersContainer.insertAdjacentHTML('afterbegin', orderCard);
     }
 
